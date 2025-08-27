2b3ed30d1d9151c607fd85bba819241388248aa5
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-08-28 02:37:10 +0500

    commit

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-function button {
-    document.getElementById("startBtn").innerHTML = "Game Started";
-    setTimeout(function() {
-        var dice= Math.floor(Math.random() * 6) + 1;
-        document.getElementById("startBtn").innerHTML = " <img  src= 'images/" + dice + ".png' alt='Dice' width='100' height='100'> ";
-    }, 1000);
 
+   function button () {
+            document.getElementById("startBtn").innerHTML = "Game Started";
 
+            setTimeout(function() {
+                var dice = Math.floor(Math.random() * 6) + 1;
 
+                document.getElementById("result").innerHTML = "<img src= 'images/"+ dice + ".png ' />" ;
 
+                var diceImg = document.getElementById("diceImage");
 
+                if (dice === 1) {
+                    diceImg.src = "images/dice1.png";
+                } else if (dice === 2) {
+                    diceImg.src = "images/dice2.png";
+                } else if (dice === 3) {
+                    diceImg.src = "images/dice3.png";
+                } else if (dice === 4) {
+                    diceImg.src = "images/dice4.png";
+                } else if (dice === 5) {
+                    diceImg.src = "images/dice5.png";
+                } else if (dice === 6) {
+                    diceImg.src = "images/dice6.png";
+                }
 
-
-
-
-
-
-
-
-
-
-}
+            }, 1000);
+        }
